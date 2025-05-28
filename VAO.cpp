2548a587ec23cf78539086ba38ceaeb21697a149
Nbartek <64834216+Nbartek@users.noreply.github.com>
2025-05-29 00:37:51 +0200

    rozgrzebane
    porawiona camera i vao, wpisane swiatło ale nie działa jescze i zaczątki animacji

--- VAO.cpp
+++ VAO.cpp
@@ -18,7 +18,13 @@ void VAO::LinkVBO(VBO& VBO, GLuint layout)
 
 	VBO.Unbind();
 }
-
+void VAO::LinkAttrib(VBO& VBO, GLuint layout, GLuint numComponents, GLenum type, GLsizeiptr stride, void* offset)
+{
+	VBO.Bind();
+	glVertexAttribPointer(layout, numComponents, type, GL_FALSE, stride, offset);
+	glEnableVertexAttribArray(layout);
+	VBO.Unbind();
+}
 void VAO::Bind()
 {
 	glBindVertexArray(ID);
